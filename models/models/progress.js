b3e7bbd2774d12a0ed7494cb9d07972f812bf9a8
MATHEW PEDER <mat.c.peder@gmail.com>
2018-12-19 01:28:17 +0000

    Add files via upload

--- models/models/progress.js
+++ models/models/progress.js
@@ -5,14 +5,13 @@ Schema for the Progress collection.
  */
 let ProgressSchema = new mongoose.Schema({
         date: {type: String, default: "2018-10-27"},
-        gender: {type: String, default: "M"},
-        age: {type: Number, default: 20},
-        weight: {type: Number, default: 70},
-        height: {type: Number, default: 70},
-        waist: {type: Number, default: 30},
+        gender: {type: String, default: ""},
+        age: {type: Number, default: 0},
+        weight: {type: Number, default: 0},
+        height: {type: Number, default: 0},
+        waist: {type: Number, default: 0}
     },
     { collection: 'progress' });
 
 module.exports = mongoose.model('Progress', ProgressSchema);
-
 
--- models/models/schedule.js
+++ models/models/schedule.js
@@ -4,36 +4,36 @@ let mongoose = require('mongoose');
 Schema for the Schedule collection which is an array for each day of the week.
  */
 let ScheduleSchema = new mongoose.Schema({
-    Weekly: [{
-        Sunday: [{
-            workout: {type: String, default: ""},
-            reps: {type: Number, default: 0}
-        }],
-        Monday: [{
-            workout: {type: String, default: ""},
-            reps: {type: Number, default: 0}
-        }],
-        Tuesday: [{
-            workout: {type: String, default: ""},
-            reps: {type: Number, default: 0}
-        }],
-        Wednesday: [{
-            workout: {type: String, default: ""},
-            reps: {type: Number, default: 0}
-        }],
-        Thursday: [{
-            workout: {type: String, default: ""},
-            reps: {type: Number, default: 0}
-        }],
-        Friday: [{
-            workout: {type: String, default: ""},
-            reps: {type: Number, default: 0}
-        }],
-        Saturday: [{
-            workout: {type: String, default: ""},
-            reps: {type: Number, default: 0}
+        Weekly: [{
+            Sunday: [{
+                workout: {type: String, default: ""},
+                reps: {type: Number, default: 0}
+            }],
+            Monday: [{
+                workout: {type: String, default: ""},
+                reps: {type: Number, default: 0}
+            }],
+            Tuesday: [{
+                workout: {type: String, default: ""},
+                reps: {type: Number, default: 0}
+            }],
+            Wednesday: [{
+                workout: {type: String, default: ""},
+                reps: {type: Number, default: 0}
+            }],
+            Thursday: [{
+                workout: {type: String, default: ""},
+                reps: {type: Number, default: 0}
+            }],
+            Friday: [{
+                workout: {type: String, default: ""},
+                reps: {type: Number, default: 0}
+            }],
+            Saturday: [{
+                workout: {type: String, default: ""},
+                reps: {type: Number, default: 0}
+            }]
         }]
-    }]
     },
     { collection: 'schedule' });
 
