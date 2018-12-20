cfced2720eead1fca965bb95cfb79df4d1da0d6d
mathew-peder <mat.c.peder@gmail.com>
2018-12-20 18:40:21 +0000

    Small changes
    Signed-off-by: mathew-peder <mat.c.peder@gmail.com>

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(function(req, res, next) { res.header("Access-Control-Allow-Origin", "*"); res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); next(); });
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -31,7 +32,6 @@ app.use(cors());
 app.use('/', indexRouter);
 app.use('/progress', progressRouter);
 app.use('/schedule', scheduleRouter);
-app.use(function(req, res, next) { res.header("Access-Control-Allow-Origin", "*"); res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); next(); });
 
 app.get('/progress', progress.findAll);
 app.get('/schedule', schedule.findAll);
