c7740c7971e1b1de2343a8f92c5b949ae3c366c4
mathew-peder <mat.c.peder@gmail.com>
2018-12-21 15:06:03 +0000

    Small changes
    Signed-off-by: mathew-peder <mat.c.peder@gmail.com>

--- models/models/progress.js
+++ models/models/progress.js
@@ -4,8 +4,8 @@ let mongoose = require('mongoose');
 Schema for the Progress collection.
  */
 let ProgressSchema = new mongoose.Schema({
-        date: {type: String, default: "2018-10-27"},
-        gender: {type: String, default: "M"},
+        date: {type: String, default: ""},
+        gender: {type: String, default: ""},
         age: {type: Number, default: 0},
         weight: {type: Number, default: 0},
         height: {type: Number, default: 0},
