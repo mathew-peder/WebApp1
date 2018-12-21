82462bdd568f2b9f3e8ef358dbf9f0b40a371a2d
mathew-peder <mat.c.peder@gmail.com>
2018-12-21 14:22:51 +0000

    Small changes
    Signed-off-by: mathew-peder <mat.c.peder@gmail.com>

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var progressRouter = require('./routes/progress');
@@ -18,16 +18,9 @@ const schedule = require("./routes/schedule");
 var app = express();
 
 // view engine setup
-app.all('*', function(req, res, next) {
-     var origin = req.get('origin');
-     res.header('Access-Control-Allow-Origin', origin);
-     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-     res.header('Access-Control-Allow-Headers', 'Content-Type');
-     next();
-});
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(function(req, res, next) { res.header("Access-Control-Allow-Origin", "*"); res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); next(); });
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -59,7 +52,6 @@ app.delete('/progress/:id', progress.deleteProgress);
 app.delete('/schedule/:id', schedule.deleteSchedule);
 
 
-app.use(function(req, res, next) { res.header("Access-Control-Allow-Origin", "*"); res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); next(); });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
